d59a460a73fc751a83d078f410954686b229ffee
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-03-04 14:56:00 +0300

    ree

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 import config from './config';
 
-const sequelize = new Sequelize(config.development);
+const sequelize = new Sequelize(config.production);
 
 export default sequelize;
