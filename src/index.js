c346ba37773b6185a2f20576f44a5bf7bb24db01
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-03-04 16:30:52 +0300

    ds

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import passport from 'passport';
+// import passport from 'passport';
 import morgan from 'morgan';
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 
-import sequelize from './database';
+// import sequelize from './database';
 import swaggerDocument from '../swagger.json';
-import mwPassport from './middleware/passport';
+// import mwPassport from './middleware/passport';
 
-import authRoute from './routes/authRoute';
-import orderRoute from './routes/orderRoute';
-import productsRoute from './routes/productsRoute';
-import userRoute from './routes/userRoute';
+// import authRoute from './routes/authRoute';
+// import orderRoute from './routes/orderRoute';
+// import productsRoute from './routes/productsRoute';
+// import userRoute from './routes/userRoute';
 
 dotenv.config();
 const app = express();
@@ -21,13 +21,13 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.use(passport.initialize());
-mwPassport(passport);
+// app.use(passport.initialize());
+// mwPassport(passport);
 
-app.use('/api/auth', authRoute);
-app.use('/api/products', productsRoute);
-app.use('/api/order', orderRoute);
-app.use('/api/user', userRoute);
+// app.use('/api/auth', authRoute);
+// app.use('/api/products', productsRoute);
+// app.use('/api/order', orderRoute);
+// app.use('/api/user', userRoute);
 
 app.get('/', (request, response) => {
   response.send('Hello from Express!');
