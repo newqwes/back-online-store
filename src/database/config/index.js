46ff2671ae4dcc2f74ca964b16cefa604befb69f
Nazar Ashyrov <nazar.ashyrov@instinctools.com>
2021-03-04 14:51:36 +0300

    ree

--- src/database/config/index.js
+++ src/database/config/index.js
@@ -8,4 +8,13 @@ module.exports = {
     port: '5432',
     omitNull: true,
   },
+  production: {
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'root',
+    database: 'store_db',
+    host: process.env.DATABASE_URL,
+    port: '5432',
+    omitNull: true,
+  },
 };
